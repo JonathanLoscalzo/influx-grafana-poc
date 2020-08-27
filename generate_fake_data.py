e03908ddfefcc38a7ba4b58dccbabf228a933654
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2020-08-27 17:06:14 -0300

    add day

--- generate_fake_data.py
+++ generate_fake_data.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 # TODO: generar data en business hours
-times = pd.bdate_range(start='2020-01-01', end='2020-08-01', tz='utc', freq='min') 
-times = times.astype(int) // 10**9
+dates = pd.bdate_range(start='2020-01-01', end='2020-08-01', tz='utc', freq='min') 
+times = dates.astype(int) // 10**9
 
 values = np.random.choice([-1,0,1],times.shape[0], p=[0.45,0.1,0.45])
 def definir_accion(d):
@@ -14,11 +14,13 @@ def definir_accion(d):
         return 'salir'
     else: 
         return '\'\''
+
 df = pd.DataFrame({"value":values, "timestamp":times})
 df["Measure"] = "Personas"
 df['device'] = 'd1'
 df['sensor'] = 'reflectivo'
 df['accion'] = df.value.apply(definir_accion)
+df['dia'] = dates.day_name()
 
 format_to="{}, device={}, sensor={}, accion={} value={}, {}"
 
@@ -26,5 +28,5 @@ format_to="{}, device={}, sensor={}, accion={} value={}, {}"
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#from-timestamps-to-epoch
 # pd.to_datetime(['2019-01-15 13:30:00']).astype(int) / 10**9
 
-df["str_format"]=df.apply(lambda x: f"{x['Measure']},device={x['device']},sensor={x['sensor']},accion={x['accion']} value={x['value']} {x['timestamp']}", axis=1)
+df["str_format"]=df.apply(lambda x: f"{x['Measure']},device={x['device']},sensor={x['sensor']},accion={x['accion']},dia={x['dia']} value={x['value']} {x['timestamp']}", axis=1)
 df.str_format.to_csv('Personas_data.txt', header=False, index=False, sep='\t', mode='a')
